45ad13f7fd540e0c5b3874bc6afe8526733a7ce8
伊藤拓海 <itoutakumi@itoutakuminoMacBook-Pro.local>
2019-08-27 15:30:09 +0900

    html,js,cssの追加

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const animationItem = document.getElementById("js-animationItem")
+
+
+animationItem.addEventListener("load",function(){
+
+    animationItem.classList.add("appear")
+
+})
+
+
+
+
+
+// function slideStart(){
+    // ②最初の画像をフェードイン
+    // ③最初の画像をフェードアウト&次の画像をフェードイン
+    // ④③の処理を一定間隔で繰り返す（setInterval）
+    // （ただし最後の画像がフェードアウトしたら最初の画像をフェードインする）
+// }
+
+
+
+// --------------------------------------------
+        // var log = function(){
+            
+        // };
+          
+        //   setInterval(log, 1000);
+
+
+
+// animationItem.classList.add
+
+// for(let i = 0; i < btns.length; i++) {
+//     btns[i].addEventListener("click",function(){
+//         window.alert(i + 1)
+//     })
+// }
+
